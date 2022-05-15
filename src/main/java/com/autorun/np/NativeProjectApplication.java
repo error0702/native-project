ff6c15eb83103fd6464bfdfd6076349a2fbf7f11
Autorun <20293349@qq.com>
2022-05-15 16:26:56 +0800

    init project

---
+++ src/main/java/com/autorun/np/NativeProjectApplication.java
@@ -0,0 +1,15 @@
+package com.autorun.np;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class NativeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeProjectApplication.class, args);
+	}
+
+}
